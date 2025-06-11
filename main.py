02d742ee4cf4edd4e0249b7c69d8e65e4e378f76
theertha-gs <gstheertha859@gmail.com>
2025-06-11 11:32:23 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel
+import os
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+key=os.getenv("GEMINI_API_KEY")
 app=FastAPI()
 '''curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=GEMINI_API_KEY" \
   -H 'Content-Type: application/json' \
@@ -16,6 +21,15 @@ app=FastAPI()
       }
     ]
   }'''
+class A(BaseModel):
+    text: str
+
+class B(BaseModel):
+    parts: List[A]
+
+class C(BaseModel):
+    contents: List[B]
+
 
 @app.get("/")
 def read_root(condition:Optional[bool]=True):
@@ -27,3 +41,9 @@ def read_root(condition:Optional[bool]=True):
 @app.get("/{id}")
 def read_root(id: int):
     return {"message":f"Hello World {id}"}
+
+@app.post("/ask")
+def ask(ques: C):
+    response= requests.post(f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={key}",json=ques.model_dump())
+    answer=response.json()
+    return answer["candidates"][0]["content"]["parts"][0]["text"]
